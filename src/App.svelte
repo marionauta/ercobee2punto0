69798a03eed6a599255cedc86f8bb84d1f456705
Mario Nachbaur <marionauta@users.noreply.github.com>
2021-01-26 17:19:17 +0100

    update workdays in 2021

--- src/App.svelte
+++ src/App.svelte
@@ -8,11 +8,19 @@
 
 	const months = [
 		{ key: 'custom', label: 'Custom' },
-		{ key: '2021/01', label: 'Enero 2021', workDays: 19 },
-		{ key: '2021/02', label: 'Febrero 2021', workDays: 20 },
-		{ key: '2021/03', label: 'Marzo 2021', workDays: 22 },
-		{ key: '2021/04', label: 'Abril 2021', workDays: 20 },
-		{ key: '2021/05', label: 'Mayo 2021', workDays: 21 },
+		{ key: '2021', label: '2021', separator: true },
+		{ key: '2021/01', label: 'Enero', workDays: 19 },
+		{ key: '2021/02', label: 'Febrero', workDays: 20 },
+		{ key: '2021/03', label: 'Marzo', workDays: 22 },
+		{ key: '2021/04', label: 'Abril', workDays: 19 },
+		{ key: '2021/05', label: 'Mayo', workDays: 21 },
+		{ key: '2021/06', label: 'Junio', workDays: 21 },
+		{ key: '2021/07', label: 'Julio', workDays: 22 },
+		{ key: '2021/08', label: 'Agosto', workDays: 21 },
+		{ key: '2021/09', label: 'Septiembre', workDays: 22 },
+		{ key: '2021/10', label: 'Octubre', workDays: 20 },
+		{ key: '2021/11', label: 'Noviembre', workDays: 21 },
+		{ key: '2021/12', label: 'Diciembre', workDays: 21 },
 	];
 
 	$: workDays = months.find(m => m.key === selectedMonth)?.workDays ?? customWorkDays;
@@ -44,7 +52,7 @@
 	<span>Mes:</span>
 	<select bind:value={selectedMonth}>
 		{#each months as month (month.key)}
-		<option value={month.key}>{month.label}</option>
+		<option value={month.key} disabled={month.separator}>{month.label}</option>
 		{/each}
 	</select>
 
@@ -52,7 +60,7 @@
 	{#if selectedMonth === 'custom'}
 	<input type=number bind:value={customWorkDays}>
 	{:else}
-	<input type=number bind:value={workDays} disabled>
+	<input type=number value={workDays} disabled>
 	{/if}
 
 	<span>Días de vacaciones:</span>
@@ -62,6 +70,9 @@
 	<input type=number bind:value={sickDays}>
 
 	<b class=center>Total: {total}€</b>
+	<a class=center href=https://www.calendarioslaborales.com/calendario-laboral-sevilla-2021.htm target=_blank>
+		Calendario laboral
+	</a>
 	<a class=center href=https://github.com/marionauta/ercobee2punto0 target=_blank>
 		github repo
 	</a>
